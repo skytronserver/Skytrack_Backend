fcf3ad1c4d5435fc3562fa91332a0cc546dac903
KishalayChakraborty <kishalaychakraborty1@gmail.com>
2024-01-11 03:58:57 +0000

    update api doc

--- Skytronsystem/Skytronsystem/settings.py
+++ Skytronsystem/Skytronsystem/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',    
     'rest_framework',
+    'drf_spectacular', 
     'skytron_api',
 ]
 
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    # ... other DRF settings ...
+}
--- Skytronsystem/Skytronsystem/urls.py
+++ Skytronsystem/Skytronsystem/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """ 
-from django.contrib import admin
+from django.contrib import admin 
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('skytron_api.urls')),  # Include the skytron_api URLs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
