f551d1657044e08bd070b05a2f8c8909997b1c0a
KishalayChakraborty <kishalaychakraborty1@gmail.com>
2024-01-18 06:35:15 +0000

    update

--- Skytronsystem/run.sh
+++ Skytronsystem/run.sh
@@ -1,6 +1,7 @@
 python3 manage.py runserver 0.0.0.0:2000 >log.log &
 
-lsof -i :2000
+#lsof -i :2000
 
-python3 manage.py makemigrations
-python3  manage.py migrate
+#python3 manage.py makemigrations
+#Spython3  manage.py migrate
+#./run.sh >logError.log &
