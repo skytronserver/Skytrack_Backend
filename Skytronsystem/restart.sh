d38c20ae937ee9c6ff46c7f296194430a6d96772
kishalaychakraborty1@gmail.com <kishalaychakraborty1@gmail.com>
2025-06-28 07:40:20 +0000

    update

--- Skytronsystem/restart.sh
+++ Skytronsystem/restart.sh
@@ -1,10 +1,20 @@
 sudo docker stop skytron-backend-api-container
 sudo docker rm skytron-backend-api-container
 sudo docker build -t skytron-backend-api -f dockerfile.api .
-sudo docker run -d  -p 2000:2000  -e  MAIL_ID=noreply@skytron.in  -e  MAIL_PW=Developer@18062025  --name skytron-backend-api-container skytron-backend-api
+#sudo docker run -d  -p 2000:2000  -e  MAIL_ID=noreply@skytron.in  -e  MAIL_PW=Developer@18062025  --name skytron-backend-api-container skytron-backend-api
 sudo docker run -d  --restart=always -p 2000:2000    -e  MAIL_ID=noreply@skytron.in  -e  MAIL_PW=Developer@18062025  --name skytron-backend-api-container skytron-backend-api 
 
  
+sudo docker stop skytron-backend-mqtt-container
+sudo docker rm skytron-backend-mqtt-container
+sudo docker build -t skytron-backend-mqtt -f dockerfile.mqtt .
+sudo docker run -d  --restart=always     -e  MAIL_ID=noreply@skytron.in  -e  MAIL_PW=Developer@18062025  --name skytron-backend-mqtt-container skytron-backend-mqtt 
+
+ 
+
+
+
+
 
 get config file from inside docker 
 
